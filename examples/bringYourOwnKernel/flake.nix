6e9d4774311a643a072dd8b58b96f4ea08df7609
Cottand <nico.dc@outlook.com>
2024-03-04 17:53:08 +0100

    try building kernel 6.8+

--- examples/bringYourOwnKernel/flake.nix
+++ examples/bringYourOwnKernel/flake.nix
@@ -11,11 +11,32 @@
       pkgs = nixpkgs.legacyPackages.${system};
       nitro = nitro-util.lib.${system};
       nitroPkgs = nitro-util.packages.${system};
+      kernel_68_rc7 = pkgs.linux.override {
+        argsOverride = {
+          src = pkgs.fetchurl {
+            url = "https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot/linux-6.8-rc7.tar.gz";
+            sha256 = "sha256-ff+VhfWcCXlDRKfikAaxcLdrRb+YgM8BHPPgoVwfB2g=";
+          };
+          version = "6.8-rc7";
+          modDirVersion = "6.8-rc7";
+        };
+      };
+      kernel_68_rc6 = pkgs.linux.override {
+        argsOverride = {
+          src = pkgs.fetchurl {
+            url = "https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot/linux-6.8-rc6.tar.gz";
+            sha256 = "sha256-GhuvgL4gfMB6ONL8NI48WWJJAPBYT+6vFS8OwGkKz74=";
+          };
+          version = "6.8.0-rc6";
+          modDirVersion = "6.8.0-rc6";
+        };
+      };
+      kernel =  kernel_68_rc6;
+      # pkgs.linux;
     in
     {
 
-      packages.aarch64-linux = rec {
-
+      packages.${system} = rec {
         myScript = pkgs.writeShellScriptBin "hello" ''
           while true;
           do
@@ -35,14 +56,14 @@
         #   paths = [ myScript ];
         # };
 
-        kernel = pkgs.linux;
+        # kernel = pkgs.linux;
 
 
         eif = nitro.mkEif {
           # use AWS' nitro-cli kernel and kernelConfig
           # inherit (nitro.blobs) kernel kernelConfig;
-          kernel = pkgs.linux + "/Image";
-          kernelConfig = pkgs.linux.configfile;
+          kernel = kernel + "/Image";
+          kernelConfig = kernel.configfile;
 
           name = "eif-hello-world";
           ramdisks = nitro.mkRamdisksFrom {
@@ -53,28 +74,45 @@
           };
         };
 
+        eif-rc = nitro.mkEif {
+          # use AWS' nitro-cli kernel and kernelConfig
+          # inherit (nitro.blobs) kernel kernelConfig;
+          kernel = kernel + "/Image";
+          kernelConfig = kernel.configfile;
+
+          name = "eif-hello-world";
+          ramdisks = nitro.mkRamdisksFrom {
+            nsmKo = nitroPkgs.nitroKernelModule.override {
+              kernel = kernel;
+            };
+            rootfs = nixpkgs.legacyPackages.${system}.hello;
+            entrypoint = "/bin/hello";
+            env = "";
+          };
+        };
+
         eif2 = nitro.mkEif {
-          # kernel = nitro.blobs.aarch64.kernel;
-          # kernelConfig = nitro.blobs.aarch64.kernelConfig;
-
-
-          kernel = pkgs.linux + "/Image";
-          kernelConfig = pkgs.linux.configfile;
+          kernel = nitro.blobs.aarch64.kernel;
+          kernelConfig = nitro.blobs.aarch64.kernelConfig;
+          # kernel = kernel + "/Image";
+          # kernelConfig = kernel.configfile;
 
           name = "eif-hello-world";
           ramdisks = nitro.mkRamdisksFrom {
             # init = nitro.blobs.aarch64.init;
+            init = nitroPkgs.eif-init;
 
 
-            # nsmKo = nitro.blobs.aarch64.nsmKo;
-            nsmKo = nitroPkgs.nitroKernelModule;
+            nsmKo = nitro.blobs.aarch64.nsmKo;
+            # nsmKo = nitroPkgs.nitroKernelModule.override {
+            #   inherit kernel;
+            # };
 
 
             rootfs = nixpkgs.legacyPackages.${system}.hello;
             entrypoint = "/bin/hello";
             env = "";
           };
-
         };
 
         tmp = pkgs.stdenv;
